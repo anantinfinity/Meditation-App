03b7e6e63531a90fd2d5745b3c63274b24fa2a66
sajidshahriar72543 <sazidshahriar39@gmail.com>
2022-10-10 15:06:07 +0600

    [FIX] bottom_nav_bar: Fix route for All Exercises button

--- lib/widgets/bottom_nav_bar.dart
+++ lib/widgets/bottom_nav_bar.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:meditation_app/constants.dart';
+import 'package:meditation_app/main.dart';
 import 'package:meditation_app/screens/kegel_ex.dart';
 import 'package:meditation_app/screens/settings.dart';
 
@@ -27,7 +28,7 @@ class BottomNavBar extends StatelessWidget {
                 context,
                 MaterialPageRoute(
                   builder: (context) =>
-                      kegel_ex(), // make screen and push to route
+                      MyApp(), // make screen and push to route
                 ),
               );
             },
